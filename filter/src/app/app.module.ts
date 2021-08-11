fc9af38209fa5aada70f5be2e4244fd8ad332cb2
aisroilov <50108113+aisroilov@users.noreply.github.com>
2021-08-11 22:32:03 +0200

    filtering products

--- filter/src/app/app.module.ts
+++ filter/src/app/app.module.ts
@@ -1,22 +1,22 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
-import { SearchResultComponent } from './components/search-result/search-result.component';
-import { SearchResultItemComponent } from './components/search-result-item/search-result-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchBoxComponent,
-    SearchResultComponent,
-    SearchResultItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Ng2SearchPipeModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
